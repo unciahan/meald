f705e76a7c9d93b7d4faf106785837652ef88bba
Yuntae Han <unciahan@gmail.com>
2022-12-17 21:08:30 +0900

    register service

---
+++ src/main/java/dotori/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package dotori.springboot.service.posts;
+
+import dotori.springboot.domain.posts.PostsRepository;
+import dotori.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
