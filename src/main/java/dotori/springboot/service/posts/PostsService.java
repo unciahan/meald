71f9bd5739bb91801321453753a67e412db41aa0
Yuntae Han <unciahan@gmail.com>
2022-12-18 18:10:02 +0900

    ui for posts service

--- src/main/java/dotori/springboot/service/posts/PostsService.java
+++ src/main/java/dotori/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package dotori.springboot.service.posts;
 
 import dotori.springboot.domain.posts.Posts;
 import dotori.springboot.domain.posts.PostsRepository;
+import dotori.springboot.web.dto.PostsListResponseDto;
 import dotori.springboot.web.dto.PostsResponseDto;
 import dotori.springboot.web.dto.PostsSaveRequestDto;
 import dotori.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,10 +34,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public Long delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+
+        return id;
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
